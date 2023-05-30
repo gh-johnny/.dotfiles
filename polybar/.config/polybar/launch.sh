44c2bb4ac6e1f9eddad1e90a2767461a327020d4
gh-johnny <jmfurtadoromero@gmail.com>
2023-05-30 03:52:30 -0700

    Log management for polybar and cron

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# Launch bar1 
+polybar bar1 2>&1 | tee $HOME/.logs/polybar_logs/bar1.log & disown
 
 echo "Bars launched..."
