c13330af2bc3b3bd8d8607abb73757152a5875e9
gh-johnny <jmfurtadoromero@gmail.com>
2023-06-01 20:08:39 -0700

    LSP(-zero) basic configuration added

---
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,22 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    --'eslint'
+})
+
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.setup()
