8aaacb843a6f9acac0dc6014cc1004187cafd5c7
gh-johnny <jmfurtadoromero@gmail.com>
2023-08-29 11:13:14 -0300

    Update: change line numbers color

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,10 +1,11 @@
--- Global function to call the colors, easier to change some specs 
+-- Global function to call the colors
 function ColorMyPencils(color)
     color = color or "catppuccin"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", {fg = "white"})
 end
 
 ColorMyPencils()
