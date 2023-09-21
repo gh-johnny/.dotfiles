1c66c9631ed7a021258e10ef3e09ce2a71a3eb9f
gh-johnny <jmfurtadoromero@gmail.com>
2023-09-21 12:45:37 -0300

    Comment remap

--- nvim/.config/nvim/after/plugin/comment.lua
+++ nvim/.config/nvim/after/plugin/comment.lua
@@ -1,3 +1,4 @@
 require('nvim_comment').setup({
-    line_mapping = "<C-l>"
+    line_mapping = "<C-l>",
+    operator_mapping = "<C-l>"
 })
--- nvim/.config/nvim/lua/config-johnny/lazy.lua
+++ nvim/.config/nvim/lua/config-johnny/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -24,7 +24,8 @@ local plugins = {
 
     -- Telescope
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
         -- or                              , branch = '0.1.1',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -38,29 +39,29 @@ local plugins = {
     -- Undotree
     "mbbill/undotree",
 
-    -- LSP 
+    -- LSP
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                          -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
         }
     },
 
-    -- Status bar 
+    -- Status bar
     -- 'feline-nvim/feline.nvim',
 
     {
@@ -69,27 +70,39 @@ local plugins = {
             "arsham/arshlib.nvim",
             "famiu/feline.nvim",
             "rebelot/heirline.nvim",
-            "kyazdani42/nvim-web-devicons",
+            -- "kyazdani42/nvim-web-devicons",
         },
         config = function()
             -- ignore any parts you don't want to use
             vim.cmd.colorscheme("arshamiser_light")
             require("arshamiser.feliniser")
             -- or:
-             require("arshamiser.heirliniser")
+            require("arshamiser.heirliniser")
 
             _G.custom_foldtext = require("arshamiser.folding").foldtext
             vim.opt.foldtext = "v:lua.custom_foldtext()"
             -- if you want to draw a tabline:
-             vim.api.nvim_set_option("tabline", [[%{%v:lua.require("arshamiser.tabline").draw()%}]])
+            vim.api.nvim_set_option("tabline", [[%{%v:lua.require("arshamiser.tabline").draw()%}]])
         end,
-   },
+    },
 
-   -- Multi cursor
-   'mg979/vim-visual-multi',
+    -- Multi cursor
+    'mg979/vim-visual-multi',
 
-   -- Comments
-   'terrortylor/nvim-comment'
+    -- Comments
+    'terrortylor/nvim-comment',
+
+    -- Trouble ( Error tab )
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    {
+        'VonHeikemen/fine-cmdline.nvim',
+        dependencies = { "MunifTanjim/nui.nvim" }
+    }
+
 
 }
 
